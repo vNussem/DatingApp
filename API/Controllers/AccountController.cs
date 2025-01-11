a874cfc2addde0b0d68ba71e811a9b618c14c5e4
vNussem <125178941+vNussem@users.noreply.github.com>
2025-01-11 16:46:28 +0200

    lesson 33

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(string userName, string password)
+    {
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = userName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+
+    }
+}
