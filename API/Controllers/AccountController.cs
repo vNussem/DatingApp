657bde48dda65755d6c8f89f820969fc85ba4c06
vNussem <125178941+vNussem@users.noreply.github.com>
2025-01-14 21:14:36 +0200

    36 - adding login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,6 +32,27 @@ public class AccountController(DataContext context) : BaseApiController
 
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x => x.UserName.ToLower() == loginDTO.Username.ToLower());
+
+        if (user == null)
+            return Unauthorized("Invalid username or password");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+                return Unauthorized("Invalid username or password");
+        }
+
+        return user;
+    }
+
     private async Task<Boolean> UserExits(string username)
     {
         return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
---
+++ API/DTOs/LoginDTO.cs
@@ -0,0 +1,10 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class LoginDTO
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
