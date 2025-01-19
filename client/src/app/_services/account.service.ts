5aa659bad13099c86d0b340205ad15cb9a95fae3
vNussem <125178941+vNussem@users.noreply.github.com>
2025-01-19 12:06:29 +0200

    52 Persisting the login

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../_model/user';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,8 +9,21 @@ import { inject, Injectable } from '@angular/core';
 export class AccountService {
   private http = inject(HttpClient);
   baseurl = 'https://localhost:5001/api/';
+  currentUser = signal<User | null>(null);
 
   login(model: any) {
-    return this.http.post(this.baseurl + 'account/login', model);
+    return this.http.post<User>(this.baseurl + 'account/login', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      }),
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { User } from './_model/user';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,26 @@ import { NavComponent } from './nav/nav.component';
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
+  accountService = inject(AccountService);
   title = 'DatingApp';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const currentUserString = localStorage.getItem('user');
+    if (!currentUserString) {
+      return;
+    }
+
+    const user: User = JSON.parse(currentUserString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: (response) => {
         this.users = response;
