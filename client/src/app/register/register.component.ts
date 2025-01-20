b6b4f3d4296f06e19a7519084a1871b363cef420
vNussem <125178941+vNussem@users.noreply.github.com>
2025-01-20 09:29:52 +0200

    56 Child to parent communication - new style

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, Output } from "@angular/core";
+import { Component, EventEmitter, input, output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 @Component({
@@ -10,7 +10,7 @@ import { FormsModule } from "@angular/forms";
 })
 export class RegisterComponent {
   usersFromHomeComponent = input.required<any>();
-  @Output() cancelRegister = new EventEmitter();
+  cancelRegister = output<boolean>();
   model: any = {};
 
   register() {
